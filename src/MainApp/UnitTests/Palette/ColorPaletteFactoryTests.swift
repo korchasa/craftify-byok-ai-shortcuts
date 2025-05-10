5a874766796e105865c7f8c4570dfc7119e82a92
korchasa <korchasa@icloud.com>
2025-05-10 17:14:40 +0300

    feat: remove ShareExtensionTests modules and related dependencies

--- src/MainApp/UnitTests/Palette/ColorPaletteFactoryTests.swift
+++ src/MainApp/UnitTests/Palette/ColorPaletteFactoryTests.swift
@@ -22,20 +22,21 @@ public final class ColorPaletteFactoryTests: XCTestCase {
     /// Проверяет цвета светлой палитры
     public func testLightPaletteColors() {
         let palette = LightMainAppColorPalette()
-        // swiftlint:disable:next prefer_nimble
         XCTAssertEqual(palette.background(), Color.white)
-        // swiftlint:disable:next prefer_nimble
-        XCTAssertEqual(palette.primaryButtonText(), Color.white)
-        // ...добавить проверки для других методов
+        XCTAssertEqual(palette.primaryButtonText(), Color.black)
+        XCTAssertEqual(palette.primaryButton(), Color.white)
+        XCTAssertEqual(palette.secondaryButtonText(), Color.black)
+        XCTAssertEqual(palette.secondaryButton(), Color.white)
     }
 
     /// Проверяет цвета тёмной палитры
     public func testDarkPaletteColors() {
         let palette = DarkMainAppColorPalette()
-        // swiftlint:disable:next prefer_nimble
-        XCTAssertEqual(palette.background(), Color(red: 0.09, green: 0.09, blue: 0.11))
-        // swiftlint:disable:next prefer_nimble
-        XCTAssertEqual(palette.primaryButtonText(), Color(red: 0.09, green: 0.09, blue: 0.11))
-        // ...добавить проверки для других методов
+        let darkBg = Color(red: 0.09, green: 0.09, blue: 0.11)
+        XCTAssertEqual(palette.background(), darkBg)
+        XCTAssertEqual(palette.primaryButtonText(), Color.white)
+        XCTAssertEqual(palette.primaryButton(), darkBg)
+        XCTAssertEqual(palette.secondaryButtonText(), Color.white)
+        XCTAssertEqual(palette.secondaryButton(), darkBg)
     }
 }
