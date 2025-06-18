94d183b7f7ba7a985af8eda3c7f03a23deacac99
korchasa <korchasa@icloud.com>
2025-06-18 20:12:46 +0300

    feat(i18n): add Italian and Bulgarian localizations

--- Project.swift
+++ Project.swift
@@ -62,6 +62,8 @@ public let project = Project(
                 "src/MainApp/Resources/pt.lproj/**",
                 "src/MainApp/Resources/de.lproj/**",
                 "src/MainApp/Resources/fr.lproj/**",
+                "src/MainApp/Resources/it.lproj/**",
+                "src/MainApp/Resources/bg.lproj/**",
                 "src/MainApp/Resources/Assets.xcassets"
             ],
             entitlements: "src/MainApp/Config/Craftify.entitlements",
@@ -143,6 +145,8 @@ public let project = Project(
                 "src/ShareExtension/Resources/pt.lproj/**",
                 "src/ShareExtension/Resources/de.lproj/**",
                 "src/ShareExtension/Resources/fr.lproj/**",
+                "src/ShareExtension/Resources/it.lproj/**",
+                "src/ShareExtension/Resources/bg.lproj/**",
                 "src/ShareExtension/Resources/Assets.xcassets"
             ],
             entitlements: "src/ShareExtension/Config/ShareExtension.entitlements",
